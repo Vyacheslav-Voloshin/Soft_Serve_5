66a59a97abd8dfa698513911ad2c81bd1c066a83
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2022-10-14 18:11:05 +0300

    Second commit

--- Solution.java
+++ Solution.java
@@ -2,7 +2,7 @@ package Java_kurs.HomeWork5;
 
 import java.io.IOException;
 
-public class Solution {
+public class  Solution {
     public static void main(String[] args) throws IOException {
         Menu menu = new Menu();
         menu.Men();
